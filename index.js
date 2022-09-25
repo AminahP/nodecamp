fa207c4d67c4d80b15f56688846911dcfedd95cf
Aminahp <aypitt7@gmail.com>
2022-09-24 20:58:10 -0400

    callback hell

---
+++ index.js
@@ -0,0 +1,36 @@
+
+//synchronous/ blocking code
+// not used b/c it takes to long. would have to wait for function to run before you can run 
+//another w/ non-blocking can do many functions at once
+
+const fs = require('fs');
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("File Written!")
+
+
+// asynchronous/ non blocking code
+
+// const fs = required('fs');
+
+fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err, data2) => {
+        console.log(data2);
+      fs.readFile(`./txt/append.txt`,'utf-8',(err, data3) => {
+         console.log(data3);
+
+         fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
+            console.log('Your file has been written 🤕');
+         })
+      });
+    });
+});
+  console.log('Will read file!');
+ 
+
+
+
