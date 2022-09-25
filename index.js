def9e5405aedfbdc8d7bf1f2ec3a37627310a14f
Aminahp <aypitt7@gmail.com>
2022-09-24 21:11:10 -0400

    nonblocking example

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-//synchronous/ blocking code
+////// ------ synchronous/ blocking code ------///////
 // not used b/c it takes to long. would have to wait for function to run before you can run 
 //another w/ non-blocking can do many functions at once
 
 const fs = require('fs');
+const http = require('http');
 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -13,24 +14,28 @@ const fs = require('fs');
 // console.log("File Written!")
 
 
-// asynchronous/ non blocking code
+/////////--- asynchronous/ non blocking code ---////////
 
 // const fs = required('fs');
 
-fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err, data2) => {
-        console.log(data2);
-      fs.readFile(`./txt/append.txt`,'utf-8',(err, data3) => {
-         console.log(data3);
+// callback hell b/c too confusing to read
+// fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
+//   if(err) return console.log('ERROR! File not found')
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err, data2) => {
+//         console.log(data2);
+//       fs.readFile(`./txt/append.txt`,'utf-8',(err, data3) => {
+//          console.log(data3);
 
-         fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
-            console.log('Your file has been written 🤕');
-         })
-      });
-    });
-});
-  console.log('Will read file!');
- 
+//          fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
+//             console.log('Your file has been written 🤕');
+//          })
+//       });
+//     });
+// });
+//   console.log('Will read file!');
+       // avaoid callback hell using async await
+
+
 
 
 
